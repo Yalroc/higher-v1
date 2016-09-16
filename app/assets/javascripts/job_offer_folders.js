d4b5176e3d9a9df4cd2bfeee320c4c443aad0e38
Guillaume Corlay <guillaume.corlay-van-der-schaal@hec.edu>
2016-09-16 13:39:38 +0200

    done with js collapsable fodlers

--- app/assets/javascripts/job_offer_folders.js
+++ app/assets/javascripts/job_offer_folders.js
@@ -2,26 +2,109 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function() {
 
-
-
-  // make the star yellow on click
-  $('.folder-js').on('click', function() {
+    // make the folders at level 0 collapsable
+    $('.tree-folder-level-0').on('click', function() {
 
         if ($(this).hasClass("folder-open")) {
           $(this).removeClass("folder-open").addClass("folder-closed");
-
-              $(this).nextUntil( ".tree-folder-level-0" ).hide()
-
+          $(this).nextUntil( ".tree-folder-level-0" ).hide()
 
         } else if ($(this).hasClass("folder-closed")) {
           $(this).removeClass("folder-closed").addClass("folder-open");
           $(this).nextUntil( ".tree-folder-level-0" ).show()
         }
-      }
-  })
+  });
 
+    // make the folders at level 1 collapsable
+    $('.tree-folder-level-1').on('click', function() {
 
+        if ($(this).hasClass("folder-open")) {
+          $(this).removeClass("folder-open").addClass("folder-closed");
+          $(this).nextUntil( ".tree-folder-level-0, .tree-folder-level-1" ).hide()
 
+        } else if ($(this).hasClass("folder-closed")) {
+          $(this).removeClass("folder-closed").addClass("folder-open");
+          $(this).nextUntil( ".tree-folder-level-1" ).show()
+        }
+    });
+
+    // make the folders at level 2 collapsable
+    $('.tree-folder-level-2').on('click', function() {
+
+        if ($(this).hasClass("folder-open")) {
+          $(this).removeClass("folder-open").addClass("folder-closed");
+          $(this).nextUntil( ".tree-folder-level-0, .tree-folder-level-1, .tree-folder-level-2" ).hide()
+
+        } else if ($(this).hasClass("folder-closed")) {
+          $(this).removeClass("folder-closed").addClass("folder-open");
+          $(this).nextUntil( ".tree-folder-level-2" ).show()
+        }
+    });
+
+    // make the folders at level 3 collapsable
+    $('.tree-folder-level-3').on('click', function() {
+
+        if ($(this).hasClass("folder-open")) {
+          $(this).removeClass("folder-open").addClass("folder-closed");
+          $(this).nextUntil( ".tree-folder-level-0, .tree-folder-level-1, .tree-folder-level-2, .tree-folder-level-3" ).hide()
+
+        } else if ($(this).hasClass("folder-closed")) {
+          $(this).removeClass("folder-closed").addClass("folder-open");
+          $(this).nextUntil( ".tree-folder-level-3" ).show()
+        }
+    });
+
+    // make the folders at level 2 collapsable
+    $('.tree-folder-level-4').on('click', function() {
+
+        if ($(this).hasClass("folder-open")) {
+          $(this).removeClass("folder-open").addClass("folder-closed");
+          $(this).nextUntil( ".tree-folder-level-0, .tree-folder-level-1, .tree-folder-level-2, .tree-folder-level-3, .tree-folder-level-4" ).hide()
+
+        } else if ($(this).hasClass("folder-closed")) {
+          $(this).removeClass("folder-closed").addClass("folder-open");
+          $(this).nextUntil( ".tree-folder-level-4" ).show()
+        }
+    });
+
+    // make the folders at level 2 collapsable
+    $('.tree-folder-level-5').on('click', function() {
+
+        if ($(this).hasClass("folder-open")) {
+          $(this).removeClass("folder-open").addClass("folder-closed");
+          $(this).nextUntil( ".tree-folder-level-0, .tree-folder-level-1, .tree-folder-level-2, .tree-folder-level-3, .tree-folder-level-4, .tree-folder-level-5" ).hide()
+
+        } else if ($(this).hasClass("folder-closed")) {
+          $(this).removeClass("folder-closed").addClass("folder-open");
+          $(this).nextUntil( ".tree-folder-level-5" ).show()
+        }
+    });
+
+    // make the folders at level 2 collapsable
+    $('.tree-folder-level-6').on('click', function() {
+
+        if ($(this).hasClass("folder-open")) {
+          $(this).removeClass("folder-open").addClass("folder-closed");
+          $(this).nextUntil( ".tree-folder-level-0, .tree-folder-level-1, .tree-folder-level-2, .tree-folder-level-3, .tree-folder-level-4, .tree-folder-level-5, .tree-folder-level-6" ).hide()
+
+        } else if ($(this).hasClass("folder-closed")) {
+          $(this).removeClass("folder-closed").addClass("folder-open");
+          $(this).nextUntil( ".tree-folder-level-6" ).show()
+        }
+    });
+
+    // make the folders at level 2 collapsable
+    $('.tree-folder-level-7').on('click', function() {
+
+        if ($(this).hasClass("folder-open")) {
+          $(this).removeClass("folder-open").addClass("folder-closed");
+          $(this).nextUntil( ".tree-folder-level-0, .tree-folder-level-1, .tree-folder-level-2, .tree-folder-level-3, .tree-folder-level-4, .tree-folder-level-5, .tree-folder-level-6, .tree-folder-level-7" ).hide()
+
+        } else if ($(this).hasClass("folder-closed")) {
+          $(this).removeClass("folder-closed").addClass("folder-open");
+          $(this).nextUntil( ".tree-folder-level-7" ).show()
+        }
+    });
 
 
 }) // end of doc.function
